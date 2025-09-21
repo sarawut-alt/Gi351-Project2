d60b008b1c979cd9b0fa0af6e18b806aa9d272f3
sakchaipatpBU <sakchai.patp@bumail.net>
2025-09-21 16:45:46 +0700

    PlayerController update / add physic mat
    + folder PhysMats
         - add 2 physic mats
    + folder Sprites
         - add 2 test sprites

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,250 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float slopeCheckDistance;
+    [SerializeField]
+    private float maxSlopeAngle;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private PhysicsMaterial2D noFriction;
+    [SerializeField]
+    private PhysicsMaterial2D fullFriction;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private float xInput;
+    private float slopeDownAngle;
+    private float slopeSideAngle;
+    private float lastSlopeAngle;
+
+    private int facingDirection = 1;
+
+    [SerializeField]
+    private bool isGrounded;
+    private bool isOnSlope;
+    [SerializeField]
+    private bool isJumping;
+    private bool canWalkOnSlope;
+    [SerializeField]
+    private bool canJump;
+
+    private Vector2 newVelocity;
+    private Vector2 newForce;
+    private Vector2 capsuleColliderSize;
+
+    private Vector2 slopeNormalPerp;
+
+    private Rigidbody2D rb;
+    private CapsuleCollider2D cc;
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CapsuleCollider2D>();
+
+        capsuleColliderSize = cc.size;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+        SlopeCheck();
+        ApplyMovement();
+    }
+
+    private void CheckInput()
+    {
+        xInput = moveAction.ReadValue<Vector2>().x;
+        //xInput = Input.GetAxisRaw("Horizontal");
+
+        if (xInput > 0 && facingDirection == -1)
+        //if (xInput == 1 && facingDirection == -1)
+        {
+            Flip();
+        }
+        else if (xInput < 0 && facingDirection == 1)
+        //else if (xInput == -1 && facingDirection == 1)
+        {
+            Flip();
+        }
+
+        if (jumpAction.triggered)
+        {
+            Jump();
+        }
+
+    }
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (rb.linearVelocity.y <= 0.0f)
+        {
+            isJumping = false;
+        }
+
+        if (isGrounded && !isJumping && slopeDownAngle <= maxSlopeAngle)
+        {
+            canJump = true;
+        }
+
+    }
+
+    private void SlopeCheck()
+    {
+        Vector2 checkPos = transform.position - (Vector3)(new Vector2(0.0f, capsuleColliderSize.y / 2));
+
+        SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
+
+        if (slopeHitFront)
+        {
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+
+        }
+        else if (slopeHitBack)
+        {
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
+
+        if (hit)
+        {
+
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeDownAngle != lastSlopeAngle)
+            {
+                isOnSlope = true;
+            }
+
+            lastSlopeAngle = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.blue);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+
+        }
+
+        if (slopeDownAngle > maxSlopeAngle || slopeSideAngle > maxSlopeAngle)
+        {
+            canWalkOnSlope = false;
+        }
+        else
+        {
+            canWalkOnSlope = true;
+        }
+
+        if (isOnSlope && canWalkOnSlope && xInput == 0.0f)
+        {
+            rb.sharedMaterial = fullFriction;
+        }
+        else
+        {
+            rb.sharedMaterial = noFriction;
+        }
+    }
+
+    private void Jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            isJumping = true;
+            newVelocity.Set(0.0f, 0.0f);
+            rb.linearVelocity = newVelocity;
+            newForce.Set(0.0f, jumpForce);
+            rb.AddForce(newForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        if (isGrounded && !isOnSlope && !isJumping) //if not on slope
+        {
+            Debug.Log("This one");
+            newVelocity.Set(movementSpeed * xInput, 0.0f);
+            rb.linearVelocity = newVelocity;
+        }
+        else if (isGrounded && isOnSlope && canWalkOnSlope && !isJumping) //If on slope
+        {
+            newVelocity.Set(movementSpeed * slopeNormalPerp.x * -xInput, movementSpeed * slopeNormalPerp.y * -xInput);
+            rb.linearVelocity = newVelocity;
+        }
+        else if (!isGrounded) //If in air
+        {
+            newVelocity.Set(movementSpeed * xInput, rb.linearVelocity.y);
+            rb.linearVelocity = newVelocity;
+        }
+
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+
+
+
+
+
+
+
+    /*public float moveSpeed = 5;
     public float jumpForce = 5;
 
     private Rigidbody2D rb;
@@ -55,7 +296,7 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = false;
         }
-    }
+    }*/
 
 
 }
