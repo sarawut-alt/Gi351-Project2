649edee047e52f0b4c23a6d785a937afc046a345
sakchaipatpBU <sakchai.patp@bumail.net>
2025-09-24 01:06:11 +0700

    update OutOfPlayZone - change player pos

--- Assets/Scripts/OutOfPlayZone.cs
+++ Assets/Scripts/OutOfPlayZone.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class OutOfPlayZone : MonoBehaviour
 {
+    public Vector3 pos;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneController.GetInstance().LoadCurrentScene();
+            PlayerController playerConntroller = other.gameObject.GetComponent<PlayerController>();
+            playerConntroller.transform.position = pos;
+            //SceneController.GetInstance().LoadCurrentScene();
         }
     }
 }
