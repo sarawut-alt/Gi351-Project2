89b458c5546d8edeb1de132a2a67f966864202aa
sakchaipatpBU <sakchai.patp@bumail.net>
2025-09-23 20:57:22 +0700

    SceneController

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,6 +6,7 @@ public class SceneController : MonoBehaviour
     public string mianScene = "MainMenu";
     public string lv1Name = "lvl1";
     public string lv2Name = "lvl2";
+    public string lv3Name = "lvl3";
 
     #region sigleton
     private static SceneController instance;
@@ -27,10 +28,23 @@ public class SceneController : MonoBehaviour
 
     public void LoadCurrentScene()
     {
-
+        string activeSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(activeSceneName);
     }
     public void LoadMainMenuScene()
     {
         SceneManager.LoadScene(mianScene);
+    }
+    public void LoadLvl1Scene()
+    {
+        SceneManager.LoadScene(lv1Name);
+    }
+    public void LoadLvl2Scene()
+    {
+        SceneManager.LoadScene(lv2Name);
+    }
+    public void LoadLvl3Scene()
+    {
+        SceneManager.LoadScene(lv3Name);
     }
 }
