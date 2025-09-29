08d696e2d33bcecff2ea8e9f80ce56061e9a316c
sakchaipatpBU <sakchai.patp@bumail.net>
2025-09-30 03:39:11 +0700

    BuffCooldownUI

--- Assets/Scripts/FinishFlag.cs
+++ Assets/Scripts/FinishFlag.cs
@@ -3,25 +3,16 @@ using UnityEngine;
 public class FinishFlag : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
+    [SerializeField] int StrawberryNeed;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
     
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             
-            if (collision.gameObject.GetComponent<PlayerController>().haveStrawberry)
+            if (collision.gameObject.GetComponent<PlayerController>().GetStrawberry() >= StrawberryNeed)
             {
                 gameManager.isWin = true;
             }
